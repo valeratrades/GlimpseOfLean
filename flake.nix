23cd80e0e32577125d676bc80c5526d1d3a33581
Valera <valeratrades@gmail.com>
2025-10-03 15:59:11 +0000

    fix: flake: proper setup

--- flake.nix
+++ flake.nix
@@ -1,39 +1,46 @@
 {
+  description = "Lean 4 Example Project";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    devenv.url = "github:cachix/devenv";
-		lean4-nix.url = "github:lenianiva/lean4-nix";
+    lean4-nix.url = "github:lenianiva/lean4-nix";
   };
 
-  outputs = inputs@{ flake-parts, nixpkgs, devenv, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-			imports = [
-        devenv.flakeModule
+  outputs = inputs @ {
+    nixpkgs,
+    flake-parts,
+    lean4-nix,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
       ];
 
-      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem = {
+        system,
+        pkgs,
+        ...
+      }: {
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          overlays = [(lean4-nix.readToolchainFile ./lean-toolchain)];
+        };
 
-      perSystem = { config, self', inputs', pkgs, system, ... }: 
-			#TODO: remove or get lean4-nix working
-        let
-					pkgs = import nixpkgs {
-						inherit system;
-						overlays = [ (inputs.lean4-nix.readToolchainFile ./lean-toolchain) ];
-					};
-        in
-        {
-					devenv.shells.default = {
-						languages.lean4 = {
-							enable = true;
-							package = pkgs.lean4;
-						};
+        packages.default =
+          ((lean4-nix.lake {inherit pkgs;}).mkPackage {
+            src = ./.;
+            roots = ["GlimpseOfLean"];
+          })
+          .executable;
 
-						enterShell = ''
-						#TODO: have it build stuff
-					lean --version
-					'';
-					};
-				};
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs.lean; [lean-all];
+        };
+      };
     };
 }
